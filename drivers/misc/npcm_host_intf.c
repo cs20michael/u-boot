4a064b81bcb542a3bf9555de4fa02333db59908d
Stanley Chu <yschu@nuvoton.com>
2025-07-08 10:13:13 +0800

    misc: npcm_host_intf: Disable pending KCS/BPC interrupts
    If there is an unhandled KCS/BPC pending interrupt after reboot,
    the KCS/BPC Linux driver may trigger interrupts immediately upon
    registering the irq. However, since the driver is not yet initialized
    to handle them, this can lead to unexpected behavior.
    
    To prevent this, disable KCS/BPC interrupts in u-boot to avoid pending
    interrupts from being raised before the Linux driver is fully initialized.
    
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/misc/npcm_host_intf.c
+++ drivers/misc/npcm_host_intf.c
@@ -40,10 +40,20 @@
 #define AUTO_HS2		BIT(12)
 #define AUTO_HS3		BIT(16)
 
+/* KCS/BPC interrupt control */
+#define BPCFEN          	0x46
+#define FRIE			BIT(3)
+#define HRIE			BIT(4)
+#define KCS1CTL			0x18
+#define KCS2CTL			0x2a
+#define KCS3CTL			0x3c
+#define IBFIE			BIT(0)
+#define OBEIE			BIT(1)
+
 static int npcm_host_intf_bind(struct udevice *dev)
 {
 	struct regmap *syscon;
-	void __iomem *base;
+	void __iomem *base, *kcs_base;
 	u32 ch_supp, val;
 	u32 ioaddr;
 	const char *type;
@@ -91,6 +101,15 @@ static int npcm_host_intf_bind(struct udevice *dev)
 
 	/* Release host wait */
 	setbits_8(SMC_CTL_REG_ADDR, SMC_CTL_HOSTWAIT);
+
+	kcs_base = dev_read_addr_index_ptr(dev, 1);
+	if (kcs_base) {
+		/* Disable KCS/BPC interrupts */
+		clrbits_8(kcs_base + BPCFEN, FRIE | HRIE);
+		clrbits_8(kcs_base + KCS1CTL, IBFIE | OBEIE);
+		clrbits_8(kcs_base + KCS2CTL, IBFIE | OBEIE);
+		clrbits_8(kcs_base + KCS3CTL, IBFIE | OBEIE);
+	}
 
 	return 0;
 }
